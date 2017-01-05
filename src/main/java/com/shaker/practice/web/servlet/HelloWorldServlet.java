7fe1d264bf3ea85ff887cf3f948455866a0eb84b
yinghuihong <123997654@qq.com>
2017-01-06 04:00:07 +0800

    Try running on tomcat

---
+++ src/main/java/com/shaker/practice/web/servlet/HelloWorldServlet.java
@@ -0,0 +1,28 @@
+package com.shaker.practice.web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by yinghuihong on 2017/1/6.
+ */
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Hello World!</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Hello World!</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
